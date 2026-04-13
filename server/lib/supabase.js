56f0cc39d9be2481040657cd6ff0852c5b2314d0
agreco123 <agreco@forbeshomes.com>
2026-04-13 11:08:23 -0400

    Delete server directory

--- server/lib/supabase.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY,
-  { db: { schema: 'market_study' } }
-);
-
-module.exports = supabase;
