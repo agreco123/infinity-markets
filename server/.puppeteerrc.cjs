7ff6a7b606e6f864a0356bf91dd0164d53bde060
agreco123 <agreco@forbeshomes.com>
2026-04-17 14:46:43 -0400

    Delete server directory

--- server/.puppeteerrc.cjs
+++
@@ -1,8 +0,0 @@
-const { join } = require('path');
-
-/** @type {import("puppeteer").Configuration} */
-module.exports = {
-  // Store Chrome in project dir so it is persisted in the Render deploy artifact
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
-
